4dcdad7bf2dcf3194e7ea88760977bed5f92e24d
Behnam <behnamsh72@gmail.com>
2021-03-15 04:54:58 +0330

    check emperical rule works for gaussian distribtions

--- dmontcarlosimulation/BNormallyDistributedData.py
+++ dmontcarlosimulation/BNormallyDistributedData.py
@@ -11,6 +11,9 @@ def normallyDistributedGenerator():
         # mean =0 and sigma=100
     weights = [1 / numSamples] * len(dist)
     print(len(weights))
+    #An array of weights, of the same shape as x. Each value in x only contributes its
+    # associated weight towards the bin count (instead of 1). If density is True, the weights are
+    # normalized, so that the integral of the density over the range remains 1.
     v = plb.hist(dist, bins=100, weights=[1 / numSamples] * len(dist))
     # the value of v is a tuple with length 2, the first element is a list or an array
     # giving me how many items are in each bin.and the second is the patches used to
@@ -18,7 +21,11 @@ def normallyDistributedGenerator():
 
     plb.xlabel('x')
     plb.ylabel('Relative Frequency')
+    #plt is a Discrete Approximation to PDF
     plb.show()
+    #If i devide 200 by 2 i get 100.which happens to be the standard deviation,so in this case what i'm
+    #going to be looking at is what fraction of the values fall within two standard deviations of the mean?
+    #kind of check on the emprerical rule
     print('Fraction within ~200 of mean =', sum(v[0][30:70]))
 
 
---
+++ dmontcarlosimulation/PDFNormalDistribution.py
@@ -0,0 +1,42 @@
+import pylab
+import scipy.integrate
+import random
+
+
+def gaussian(x, mu, sigma):
+    factor1 = (1.0 / (sigma * ((2 * pylab.pi) ** 0.5)))
+    factor2 = pylab.e ** -(((x - mu) ** 2) / (2 * sigma ** 2))
+    return factor1 * factor2
+
+
+def checkEmpirical(numTrials):
+    for t in range(numTrials):
+        mu = random.randint(-10, 10)
+        sigma = random.randint(1, 10)
+        print("For mu= ", mu, 'and sigma =', sigma)
+        for numStd in (1, 1.96, 3):
+            area = scipy.integrate.quad(gaussian, mu - numStd * sigma, mu + numStd * sigma, (mu, sigma))[0]
+            print('Fraction within ', numStd, ' std = ', round(area, 4))
+
+
+def testGaussian():
+    xVals, yVals = [], []
+    mu, sigma = 0, 1
+    x = -4
+    while x <= 4:
+        xVals.append(x)
+        yVals.append(gaussian(x, mu, sigma))
+        x += 0.05
+
+    pylab.plot(xVals, yVals)
+    pylab.title('Normal Distribution ,mu=' + str(mu) + ', sigma = ' + str(sigma))
+    pylab.show()
+
+
+def testCheckEmpirical(numTrials):
+    checkEmpirical(numTrials)
+
+
+if __name__ == "__main__":
+    # testGaussian()
+    testCheckEmpirical(3)
