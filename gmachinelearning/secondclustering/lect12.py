fc13315879f8b224310c80537c56a7bdba85314a
Behnam <behnamsh72@gmail.com>
2021-07-01 01:43:35 +0430

    lec 11 & lec 12 examples has done

---
+++ gmachinelearning/secondclustering/lect12.py
@@ -0,0 +1,158 @@
+import cluster
+import random, pylab, numpy
+
+
+# subtype of Example
+class Patient(cluster.Example):
+    pass
+
+
+# this is a method called z-scaling if for example one feature is weight and i clustered them
+# well weight would always completely dominate the Euclidean distance Because the dynamic range of the weights
+# in this room is much higher than the dynamic range of 0 to 1.and so for the reptiles in in previous lectures
+# we change number of legs to  a binary variable but maybe we don't wanna make weight a binary variable.
+# because it's something we want to take into account.so what we do is we scale it.so this is a method called
+# Z scaling More general than just making 0 or 1.it takes in all of the values of specific feature and then perform
+# some simple calculations and then when it's done,the resulting array  it returns has a known mean and a known
+# standard deviation.So what's the mean going to  be?it's always going to be the same thing,independent of the initial
+# values. the mean always is 0 and the standard deviation a little harder to figure,but it will always be 1.
+# The other way people scale is interpolate.They take the smallest value and call it 0, the biggest value they call it 1.
+# and then they do  a linear interpolation of all the values betwen 0 and 1.So the range is 0 to 1.
+def scaleAttrs(vals):
+    vals = pylab.array(vals)
+    mean = sum(vals) / len(vals)
+    sd = numpy.std(vals)
+    vals = vals - mean
+    return vals / sd
+
+
+def getData(toScale=False):
+    # read in data
+    hrList, stElevList, ageList, prevACSList, classList = [], [], [], [], []
+    cardiacData = open('cardiacData.txt', 'r')
+    for l in cardiacData:
+        l = l.split(',')
+        hrList.append(int(l[0]))
+        stElevList.append(int(l[1]))
+        ageList.append(int(l[2]))
+        prevACSList.append(int(l[3]))
+        classList.append(int(l[4]))
+    if toScale:
+        hrList = scaleAttrs(hrList)
+        stElevList = scaleAttrs(stElevList)
+        ageList = scaleAttrs(ageList)
+        prevACSList = scaleAttrs(prevACSList)
+    # Build points
+    points = []
+    for i in range(len(hrList)):
+        features = pylab.array([hrList[i], prevACSList[i], \
+                                stElevList[i], ageList[i]])
+        pIndex = str(i)
+        points.append(Patient('P' + pIndex, features, classList[i]))
+    return points
+
+
+def kmeans(examples, k, verbose=False):
+    # Get k randomly chosen initial centroids, create cluster for each
+    initialCentroids = random.sample(examples, k)
+    clusters = []
+    for e in initialCentroids:
+        clusters.append(cluster.Cluster([e]))
+
+    # Iterate until centroids do not change
+    converged = False
+    numIterations = 0
+    while not converged:
+        numIterations += 1
+        # Create a list containing k distinct empty lists
+        newClusters = []
+        for i in range(k):
+            newClusters.append([])
+
+        # Associate each example with closest centroid
+        for e in examples:
+            # Find the centroid closest to e
+            smallestDistance = e.distance(clusters[0].getCentroid())
+            index = 0
+            for i in range(1, k):
+                distance = e.distance(clusters[i].getCentroid())
+                if distance < smallestDistance:
+                    smallestDistance = distance
+                    index = i
+            # Add e to the list of examples for appropriate cluster
+            newClusters[index].append(e)
+
+        for c in newClusters:  # Avoid having empty clusters
+            if len(c) == 0:
+                raise ValueError('Empty Cluster')
+
+        # Update each cluster; check if a centroid has changed
+        converged = True
+        for i in range(k):
+            if clusters[i].update(newClusters[i]) > 0.0:
+                converged = False
+        if verbose:
+            print('Iteration #' + str(numIterations))
+            for c in clusters:
+                print(c)
+            print('')  # add blank line
+    return clusters
+
+
+def trykmeans(examples, numClusters, numTrials, verbose=False):
+    """Calls kmeans numTrials times and returns the result with the
+          lowest dissimilarity"""
+    best = kmeans(examples, numClusters, verbose)
+    minDissimilarity = cluster.dissimilarity(best)
+    trial = 1
+    while trial < numTrials:
+        try:
+            clusters = kmeans(examples, numClusters, verbose)
+        except ValueError:
+            continue  # If failed, try again
+        currDissimilarity = cluster.dissimilarity(clusters)
+        if currDissimilarity < minDissimilarity:
+            best = clusters
+            minDissimilarity = currDissimilarity
+        trial += 1
+    return best
+
+
+def printClustering(clustering):
+    """Assumes: clustering is a sequence of clusters
+       Prints information about each cluster
+       Returns list of fraction of pos cases in each cluster"""
+    posFracs = []
+    for c in clustering:
+        numPts = 0
+        numPos = 0
+        for p in c.members():
+            numPts += 1
+            if p.getLabel() == 1:
+                numPos += 1
+        fracPos = numPos / numPts
+        posFracs.append(fracPos)
+        print('Cluster of size', numPts, 'with fraction of positives =',
+              round(fracPos, 4))
+    return pylab.array(posFracs)
+
+
+def testClustering(patients, numClusters, seed=0, numTrials=5):
+    random.seed(seed)
+    bestClustering = trykmeans(patients, numClusters, numTrials)
+    posFracs = printClustering(bestClustering)
+    return posFracs
+
+
+if __name__ == "__main__":
+    patients = getData(True)
+    for k in (2,4,6):
+        print('\n     Test k-means (k = ' + str(k) + ')')
+        posFracs = testClustering(patients, k, 2)
+
+    numPos = 0
+    for p in patients:
+        if p.getLabel() == 1:
+            numPos += 1
+    print('Total number of positive patients =', numPos)
+
